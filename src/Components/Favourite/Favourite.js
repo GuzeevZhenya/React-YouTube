38afe2916c78b73e70c60807a390f5ba78c3dddd
GuzeevZhenya <shyst.evgeny@mail.ru>
2022-05-11 12:40:12 +0300

    done

--- src/Components/Favourite/Favourite.js
+++ src/Components/Favourite/Favourite.js
@@ -1,51 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { useSelector, useDispatch } from 'react-redux';
-import './Favourite.css';
-import { FavouriteForm } from '../Form/Form';
-import { Form } from '../Form/Form';
+import { useSelector, useDispatch } from "react-redux";
+import "./Favourite.css";
+import { FavouriteForm } from "../Form/Form";
+import { Form } from "../Form/Form";
 
 export default function Favourite() {
   const dispatch = useDispatch();
   const searchReducer = useSelector((state) => state.videoReducer);
   // const [ favourite, setFavourite ] = useState(searchReducer.formDate);
 
-  const [ showForm, setShowForm ] = useState(false);
-  const [showFavouriteForm,setShowFavouriteForm] = useState(false)
-  const [ formInfo, setFormInfo ] = useState(null);
+  const [showForm, setShowForm] = useState(false);
+  const [showFavouriteForm, setShowFavouriteForm] = useState(false);
+  const [formInfo, setFormInfo] = useState(null);
 
   const info = (e) => {
     setShowFavouriteForm(true);
     const itemInfo = searchReducer.formDate.filter((item) => e === item.id);
     setFormInfo(itemInfo);
   };
- 
- 
 
   const removeFavouriteVideo = (id) => {
-    dispatch({ type: 'REMOVE_VIDEO', value: id });
+    dispatch({ type: "REMOVE_VIDEO", value: id });
   };
 
   const changeInfo = (id) => {
     setShowForm(!showForm);
     const itemInfo = searchReducer.formDate.filter((item) => id === item.id);
     setFormInfo(itemInfo);
-  }
+  };
 
-  
   const favouriteDate =
     searchReducer.formDate &&
     searchReducer.formDate.map((item) => (
       <div>
         <ul className="favourite__block">
-          <li id={item.id} className="favourite__item" onClick={(e) => info(e.target.id)}>
+          <li
+            id={item.id}
+            className="favourite__item"
+            onClick={(e) => info(e.target.id)}
+          >
             {item.formName}
           </li>
           <div>
-            <button className="favourite__button favourite__button--change" onClick={()=>changeInfo(item.id)}>Изменить</button>
+            <button
+              className="favourite__button favourite__button--change"
+              onClick={() => changeInfo(item.id)}
+            >
+              Изменить
+            </button>
             <button
               className="favourite__button favourite__button--remove"
-              onClick={() => removeFavouriteVideo(item.id)}>
+              onClick={() => removeFavouriteVideo(item.id)}
+            >
               Удалить
             </button>
           </div>
@@ -55,11 +62,22 @@ export default function Favourite() {
   return (
     <div>
       <h1>Избранное</h1>
-      {showForm ? <Form setShowForm={setShowForm} changeValue={true} formInfo={formInfo}/> : null}
+      {showForm ? (
+        <Form
+          setShowForm={setShowForm}
+          changeValue={true}
+          formInfo={formInfo}
+        />
+      ) : null}
       {favouriteDate}
-      {showFavouriteForm ? <FavouriteForm setShowFavouriteForm={setShowFavouriteForm} formInfo={formInfo} /> : null}
+      {showFavouriteForm ? (
+        <FavouriteForm
+          setShowFavouriteForm={setShowFavouriteForm}
+          formInfo={formInfo}
+        />
+      ) : null}
     </div>
   );
 }
 
-// 
+//
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,16 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Route, NavLink } from 'react-router-dom';
-import { videoApi } from '../../api';
-import './Form.css';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Route, NavLink } from "react-router-dom";
+import { videoApi } from "../../api";
+import "./Form.css";
 
-export const Form = ({ setShowForm,formInfo,changeValue }) => {
-  const [ sortingType, setSortingType ] = useState(formInfo === undefined ? 'date' : formInfo[0].sortingType);
-  const [ formName, setFormName ] = useState(formInfo === undefined ? null : formInfo[0].formName);
+export const Form = ({ setShowForm, formInfo, changeValue }) => {
+  const [sortingType, setSortingType] = useState(
+    formInfo === undefined ? "date" : formInfo[0].sortingType
+  );
+  const [formName, setFormName] = useState(
+    formInfo === undefined ? null : formInfo[0].formName
+  );
   const [rangeValue, setRangeValue] = useState(4);
-  const [id, setId] = useState(formInfo === undefined ? '' : formInfo[0].id );
-  
-  
+  const [id, setId] = useState(formInfo === undefined ? "" : formInfo[0].id);
+
   // const [ request, setRequest ] = useState();
 
   // const [ nameIsValid, setNameIsValid ] = useState(false);
@@ -18,32 +21,40 @@ export const Form = ({ setShowForm,formInfo,changeValue }) => {
   const dispatch = useDispatch();
   const searchReducer = useSelector((state) => state.videoReducer);
 
-  const [request, setReques] = useState(formInfo === undefined ? searchReducer.searchFilm : formInfo[0].request);
+  const [request, setReques] = useState(
+    formInfo === undefined ? searchReducer.searchFilm : formInfo[0].request
+  );
 
-  console.log(request)
-  
+  console.log(request);
 
   const setFormDate = (e) => {
     e.preventDefault();
     const randomId = `f${(~~(Math.random() * 1e8)).toString(16)}`;
-    setId(randomId)
+    setId(randomId);
     if (changeValue) {
-      dispatch({type:'CHANGE_VALUE',value: { sortingType, request: request, formName, rangeValue, id }})
+      dispatch({
+        type: "CHANGE_VALUE",
+        value: { sortingType, request: request, formName, rangeValue, id },
+      });
     } else {
       dispatch({
-        type: 'SET_DATA',
-        value: { sortingType, request: searchReducer.searchFilm, formName, rangeValue, id: randomId }
+        type: "SET_DATA",
+        value: {
+          sortingType,
+          request: searchReducer.searchFilm,
+          formName,
+          rangeValue,
+          id: randomId,
+        },
       });
     }
-  
-  
+
     setShowForm(false);
   };
 
   const closeForm = (e) => {
     setShowForm(false);
   };
-  
 
   return (
     <div>
@@ -53,33 +64,35 @@ export const Form = ({ setShowForm,formInfo,changeValue }) => {
           <label>
             <span>Запрос</span>
             <input
-              className={changeValue ? '' : 'input-disabled'}
+              className={changeValue ? "" : "input-disabled"}
               disabled={changeValue ? false : true}
               value={changeValue ? request : searchReducer.searchFilm}
               placeholder="чем кормить кота"
-              onChange={(e)=>setReques(e.target.value)}
+              onChange={(e) => setReques(e.target.value)}
             />
           </label>
           <label>
             <span>*Название</span>
             <span className="form-validation">
-              {formName && formName.length < 3 ? 'Название должно иметь больше 2х символов' : null}
-            
+              {formName && formName.length < 3
+                ? "Название должно иметь больше 2х символов"
+                : null}
             </span>
             <input
               placeholder="Укажите название"
               onChange={(e) => setFormName(e.target.value)}
-              className={formName && formName.length < 3 ? 'error' : null}
+              className={formName && formName.length < 3 ? "error" : null}
               value={formName}
             />
           </label>
           <label>
-             <span>Сортировать по</span>
-              <select
+            <span>Сортировать по</span>
+            <select
               value={sortingType}
               onChange={(e) => setSortingType(e.target.value)}
               className="form-sorting"
-              name="form-sorting">
+              name="form-sorting"
+            >
               <option value="date">Дате</option>
               <option value="raiting">Рейтенгу</option>
               <option value="rolevant">Ролевантности</option>
@@ -99,15 +112,19 @@ export const Form = ({ setShowForm,formInfo,changeValue }) => {
             <span>{rangeValue}</span>
           </label>
         </div>
-      
+
         <div className="form-buttons">
-          <button onClick={() => closeForm()} className="form-button button-dns">
+          <button
+            onClick={() => closeForm()}
+            className="form-button button-dns"
+          >
             Не Сохранять
           </button>
           <button
             onClick={(e) => setFormDate(e)}
             className="form-button button-save"
-            disabled={formName && formName.length >= 3 ? false : true}>  
+            disabled={formName && formName.length >= 3 ? false : true}
+          >
             Сохранить
           </button>
         </div>
@@ -124,34 +141,38 @@ export const FavouriteForm = ({ formInfo, setShowFavouriteForm }) => {
     setShowFavouriteForm(false);
   };
 
-  const favouriteRequest = formInfo && formInfo.map(item => (
-    <div className="favouriteForm__info" key={item.id} >
-      <span>Название: «{item.formName}»</span>
-      <span>Запрос: «{item.request}»</span>
-      <span>Сортировка: «{item.sortingType}»</span>
-      <span>Max количество видео: «{item.rangeValue}»</span>
-    </div>  
-  ))
- 
+  const favouriteRequest =
+    formInfo &&
+    formInfo.map((item) => (
+      <div className="favouriteForm__info" key={item.id}>
+        <span>Название: «{item.formName}»</span>
+        <span>Запрос: «{item.request}»</span>
+        <span>Сортировка: «{item.sortingType}»</span>
+        <span>Max количество видео: «{item.rangeValue}»</span>
+      </div>
+    ));
 
   const getRequest = () => {
-    dispatch({type:'SEARCH_VIDEO',value:formInfo[0].request})
+    dispatch({ type: "SEARCH_VIDEO", value: formInfo[0].request });
     videoApi
       .getVideo(formInfo[0].request, formInfo[0].rangeValue)
-      .then((data) => dispatch({ type: 'ADD_VIDEO', value: data }));
+      .then((data) => dispatch({ type: "ADD_VIDEO", value: data }));
   };
   return (
     <div className="favouriteForm">
-        {favouriteRequest}
+      {favouriteRequest}
       <div className="form-buttons">
         <button onClick={() => closeForm()} className="form-button button-dns">
           Не выполнять
         </button>
-        <NavLink to={`/Main`} onClick={(e) => getRequest(e)} className="form-button button-save">
+        <NavLink
+          to={`/Main`}
+          onClick={(e) => getRequest(e)}
+          className="form-button button-save"
+        >
           Выполнить
         </NavLink>
       </div>
     </div>
   );
 };
-
